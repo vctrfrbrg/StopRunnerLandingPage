62e4a974c7d22a7d05d96314683793b1749edb3c
vctrfrbrg <victor@frbrg.se>
2023-07-22 15:38:21 +0200

    Move files to work locally too

--- app/src/components/AboutTheGame.jsx
+++ app/src/components/AboutTheGame.jsx
@@ -3,11 +3,6 @@ import { Box, Typography } from '@mui/material';
 export const AboutTheGame = () => {
   return (
     <Box p={2} display="flex" flexDirection="column" alignItems="center" justifyContent="center" minHeight="720px">
-      {/* <img 
-        src="/path/to/your/image.jpg" 
-        alt="Your game image" 
-        style={{ width: 200, height: 100, objectFit: 'cover' }}
-      /> */}
       <Typography variant="h6" align="center">
         You run, you stop, you win!
       </Typography>
